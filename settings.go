3f10811e4ca2470cf31fad09fdad06f609c79530
Josh Ferrara <scjosh2@gmail.com>
2018-09-11 16:01:55 -0700

    Minor settings.go change to fix notifications from relay -> streampush.

--- settings.go
+++ settings.go
@@ -3,6 +3,5 @@ package main
 const (
 	RTMP_LISTEN   = ":1935"
 	API_LISTEN    = ":8888"
-	NOTIFY_URL    = "http://localhost/api/v1/notify"
-	WS_NOTIFY_URL = "ws://localhost/ws/notify"
+	NOTIFY_URL    = "http://streampush:8000/api/v1/notify"
 )
